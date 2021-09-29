bbb00753102a3d21ce9a4d3d92523ac75dc969c0
Jordan Evens <jordan.evens@gmail.com>
2021-09-29 15:09:41 -0400

    fixed slope being read incorrectly and using degrees instead of % slope

--- src/cpp/ConstantGrid.h
+++ src/cpp/ConstantGrid.h
@@ -166,8 +166,8 @@ public:
   {
     logging::info("Reading file %s", filename.c_str());
 #ifndef NDEBUG
-    V min_value = std::numeric_limits<V>::max();
-    V max_value = std::numeric_limits<V>::min();
+    auto min_value = std::numeric_limits<int16_t>::max();
+    auto max_value = std::numeric_limits<int16_t>::min();
 #endif
     const GridBase grid_info = read_header<T>(tif, gtif);
     const auto coordinates = grid_info.findFullCoordinates(point, false);
@@ -229,7 +229,7 @@ public:
           {
             const auto cur_hash = static_cast<HashSize>(i) * MAX_COLUMNS + w + x + offset_x;
             const auto offset = y * tile_length + x;
-            V cur = *(static_cast<V*>(buf) + offset);
+            auto cur = *(static_cast<int16_t*>(buf) + offset);
 #ifndef NDEBUG
             min_value = min(cur, min_value);
             max_value = max(cur, max_value);
