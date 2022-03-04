e808a71daa11f43d069cf1c66542a264300d54b6
Jordan Evens <jordan.evens@gmail.com>
2022-03-04 08:29:58 -0500

    CHANGE: changed InnerPos to just use doubles without sub_x & sub_y

--- src/cpp/ConvexHull.cpp
+++ src/cpp/ConvexHull.cpp
@@ -13,8 +13,8 @@ distPtPt(
   fs::sim::InnerPos& b
 )
 {
-  const auto abX = (b.sub_x - a.sub_x);
-  const auto abY = (b.sub_y - a.sub_y);
+  const auto abX = (b.x - a.x);
+  const auto abY = (b.y - a.y);
   return (abX * abX + abY * abY);
 }
 
@@ -26,20 +26,20 @@ hull(
   set<fs::sim::InnerPos> hullPoints{};
   double maxX = std::numeric_limits<double>::min();
   double minX = std::numeric_limits<double>::max();
-  fs::sim::InnerPos maxPos{0, 0, 0, 0};
-  fs::sim::InnerPos minPos{0, 0, 0, 0};
+  fs::sim::InnerPos maxPos{minX, minX};
+  fs::sim::InnerPos minPos{maxX, maxX};
 
   for (const auto p : a)
   {
-    if (p.sub_x > maxX)
+    if (p.x > maxX)
     {
-      maxX = p.sub_x;
+      maxX = p.x;
       maxPos = p;
     }
     // don't use else if because first point should be set for both
-    if (p.sub_x < minX)
+    if (p.x < minX)
     {
-      minX = p.sub_x;
+      minX = p.x;
       minPos = p;
     }
   }
@@ -77,12 +77,12 @@ quickHull(
   fs::logging::warning("Checking %d points", a->size());
 #endif
   double maxD = -1.0;   // just make sure it's not >= 0
-  fs::sim::InnerPos maxPos{0, 0, 0, 0};
+  fs::sim::InnerPos maxPos{std::numeric_limits<double>::min(), std::numeric_limits<double>::min()};
   vector<fs::sim::InnerPos> usePoints{};
 
   // since we do distLinePt so often, calculate the parts that are always the same
-  const auto abX = (n2.sub_x - n1.sub_x);
-  const auto abY = (n2.sub_y - n1.sub_y);
+  const auto abX = (n2.x - n1.x);
+  const auto abY = (n2.y - n1.y);
   /* so instead of:
    * return ( (b->x - a->x)*(a->y - p->y) - (a->x - p->x)*(b->y - a->y) );
    * we can do the equivalent of:
@@ -93,7 +93,7 @@ quickHull(
   for (const auto p : a)
   {
     // loop through points, looking for furthest
-    const auto d = (abX * (n1.sub_y - p.sub_y) - (n1.sub_x - p.sub_x) * abY);
+    const auto d = (abX * (n1.y - p.y) - (n1.x - p.x) * abY);
     if (d >= 0)
     {
       if (d > maxD)
@@ -103,7 +103,7 @@ quickHull(
       }
       // only use in next step if on positive side of line
 #ifdef DEBUG_HULL
-      fs::logging::warning("Adding point (%d, %d) (%f, %f)", p.x, p.y, p.sub_x, p.sub_y);
+      fs::logging::warning("Adding point (%d, %d) (%f, %f)", p.x, p.y, p.x, p.y);
 #endif
       usePoints.emplace_back(p);
     }
@@ -122,8 +122,8 @@ quickHull(
       "Remove did not get rid of point (%d, %d) (%f, %f)",
       maxPos.x,
       maxPos.y,
-      maxPos.sub_x,
-      maxPos.sub_y
+      maxPos.x,
+      maxPos.y
     );
 #endif
     // need to figure out which direction we're going in
@@ -171,8 +171,8 @@ quickHull(
       "Remove did not get rid of point (%d, %d) (%f, %f)",
       maxPos.x,
       maxPos.y,
-      maxPos.sub_x,
-      maxPos.sub_y
+      maxPos.x,
+      maxPos.y
     );
     fs::logging::check_fatal(
       usePoints->size() == a->size(),
--- src/cpp/Scenario.cpp
+++ src/cpp/Scenario.cpp
@@ -261,7 +261,7 @@ Scenario::evaluate(
       break;
     case Event::NEW_FIRE:
       // HACK: don't do this in constructor because scenario creates this in its constructor
-      points_[p].emplace_back(p.column(), p.row(), CELL_CENTER, CELL_CENTER);
+      points_[p].emplace_back(p.column() + CELL_CENTER, p.row() + CELL_CENTER);
       if (fuel::is_null_fuel(event.cell()))
       {
         log_fatal("Trying to start a fire in non-fuel");
@@ -544,7 +544,7 @@ Scenario::run(
       const auto cell = env.cell(location);
       log_check_fatal(fuel::is_null_fuel(cell), "Null fuel in perimeter");
       log_verbose("Adding point (%d, %d)", cell.column() + CELL_CENTER, cell.row() + CELL_CENTER);
-      points_[cell].emplace_back(cell.column(), cell.row(), CELL_CENTER, CELL_CENTER);
+      points_[cell].emplace_back(cell.column() + CELL_CENTER, cell.row() + CELL_CENTER);
     }
     addEvent(Event::makeFireSpread(start_time_));
   }
