9c523e5d0d5d6ea02d6d7df8ac0669b82067094f
Jordan Evens <jordan.evens@gmail.com>
2022-04-07 08:55:15 -0400

    changed to use nullptr instead of NULL

--- src/cpp/Scenario.cpp
+++ src/cpp/Scenario.cpp
@@ -195,11 +195,11 @@ Scenario::reset(
   extinction_thresholds_.resize(num);
   spread_thresholds_by_ros_.resize(num);
   // if these are null then all probability thresholds remain 0
-  if (NULL != mt_extinction)
+  if (nullptr != mt_extinction)
   {
     make_threshold(&extinction_thresholds_, mt_extinction, start_day_, last_date_);
   }
-  if (NULL != mt_spread)
+  if (nullptr != mt_spread)
   {
     make_threshold(
       &spread_thresholds_by_ros_,
