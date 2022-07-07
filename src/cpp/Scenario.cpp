8b564c7fcebd276baa14607d5807d9d3b4b386c3
Jordan Evens <jordan.evens@gmail.com>
2022-07-07 10:06:12 -0400

    CHANGE: changed so CELL_CENTER = 0.5

--- src/cpp/Scenario.cpp
+++ src/cpp/Scenario.cpp
@@ -11,8 +11,7 @@
 #include "ConvexHull.h"
 namespace fs::sim
 {
-// FIX: why is this not just 0.5?
-constexpr auto CELL_CENTER = 0.5555555555555555;
+constexpr auto CELL_CENTER = 0.5;
 constexpr auto PRECISION = 0.001;
 static atomic<size_t> COUNT = 0;
 static atomic<size_t> COMPLETED = 0;
