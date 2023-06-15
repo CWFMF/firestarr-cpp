fd77f3c3501a4f6fbc0b2930573d343ecf132508
Jordan Evens <jordan.evens@gmail.com>
2023-06-15 17:59:43 -0400

    fixed leaf on/off being applied in reverse for fuels

--- src/cpp/FBP45.h
+++ src/cpp/FBP45.h
@@ -1367,7 +1367,8 @@ find_fuel_by_season(
   const FuelVariable<FuelSpring, FuelSummer>& fuel
 ) noexcept
 {
-  return calculate_is_green(nd) ? fuel.spring() : fuel.summer();
+  // if not green yet, then still in spring conditions
+  return calculate_is_green(nd) ? fuel.summer() : fuel.spring();
 }
 template <class FuelSpring, class FuelSummer>
 [[nodiscard]] double
