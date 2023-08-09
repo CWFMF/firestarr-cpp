9230c6acf0fe33f335c9d6e9544d99ad5100a575
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-08-09 13:48:04 -0400

    change to use try_emplace()

--- src/cpp/FireWeatherDaily.cpp
+++ src/cpp/FireWeatherDaily.cpp
@@ -480,13 +480,9 @@ make_wx(
     wx.dmc(),
     wx.dc()
   );
-  const auto seek = all_weather.find(result);
-  if (seek != all_weather.end())
-  {
-    return &*seek;
-  }
-  all_weather.insert(result);
-  return &*all_weather.find(result);
+  const auto& wx_inserted = all_weather.insert(result);
+  // doesn't matter if was already there or just inserted
+  return &(*(wx_inserted.first));
 }
 static const FwiWeather*
 make_wx(
