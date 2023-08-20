29069e4e6627795219ad04066a6411ee6fd62d67
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-08-20 08:15:21 -0400

    add build scripts

---
+++ scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# didn't figure out how to do this with cmake yet but this works for now
+DIR_ROOT="$(realpath $(dirname $(realpath "$0"))/..)"
+DIR_BUILD="${DIR_ROOT}/build"
+VARIANT="$1"
+if ( [ -z "${VARIANT}" ] || ( [ "Release" != "${VARIANT}" ] && [ "Debug" != ${VARIANT} ] ) ); then
+  # assume that argument is an arg to pass to cmake
+  VARIANT=Release
+  # don't shift because $1 wasn't the variant
+else
+  shift;
+fi
+CLEAN_FIRST=
+if [ "--clean-first" == "$1" ]; then
+  CLEAN_FIRST="--clean-first"
+  # HACK: if used different args for config then just --clean-first doesn't change that
+  rm -rf "${DIR_BUILD}"
+  shift;
+fi
+echo Set VARIANT=${VARIANT}
+echo "${@}"
+/usr/bin/cmake --no-warn-unused-cli ${@} -DCMAKE_BUILD_TYPE:STRING=${VARIANT} -S${DIR_ROOT} -B${DIR_BUILD} -G "Unix Makefiles" \
+  && /usr/bin/cmake --build ${DIR_BUILD} --config ${VARIANT} --target all -j 50 ${CLEAN_FIRST} --
