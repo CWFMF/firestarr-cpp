51518ca695b2b5d0fe63be73f45fabae5943892b
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-06-09 07:46:21 -0400

    moved code into merge() and merge_all()

--- src/cpp/Scenario.cpp
+++ src/cpp/Scenario.cpp
@@ -157,10 +157,7 @@ public:
   )
     : PointSourceMap()
   {
-    do_par(points_and_sources, [this](const auto& pr) {
-      points_merge(pr.points_map_);
-      sources_merge(pr.sources_map_);
-    });
+    merge_all(points_and_sources);
   }
   PointSourceMap(
     const topo::Cell location,
@@ -171,6 +168,7 @@ public:
   {
     // no need to lock since this doesn't exist yet
     points_merge_values_(p_o);
+    // if we do sources second we only need to find relativeIndex once per key
     do_each(points_map_, [this, &location](const auto& kv) {
       const auto& for_cell = kv.first;
       const auto source = relativeIndex(for_cell, location);
@@ -204,6 +202,23 @@ public:
     });
   }
 private:
+  void
+  merge_all(
+    auto& points_and_sources
+  )
+  {
+    do_par(points_and_sources, [this](const auto& pr) {
+      merge(pr);
+    });
+  }
+  void
+  merge(
+    const PointSourceMap& pr
+  )
+  {
+    points_merge(pr.points_map_);
+    sources_merge(pr.sources_map_);
+  }
   template <class L>
   inline void
   points_merge_values(
