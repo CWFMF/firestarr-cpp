5fb7eb64bec96dae0c3eb66cdb15dee569868412
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-06-13 11:42:39 -0400

    use for loop instead of manual iterator loop

--- src/cpp/InnerPos.h
+++ src/cpp/InnerPos.h
@@ -147,14 +147,11 @@ apply_offsets(
 {
   // apply offsets to point
   std::map<Location, OffsetSet> r{};
-  const Offset* out = &(offsets[0]);
-  // this is an invalid point to after array we can use as a guard
-  const Offset* e = &(offsets[offsets.size()]);
-  while (out != e)
+  for (const auto& out : offsets)
   {
-    const double x_o = duration * out->x();
-    const double y_o = duration * out->y();
-    for (auto& p : pts)
+    const double x_o = duration * out.x();
+    const double y_o = duration * out.y();
+    for (const auto& p : pts)
     {
       // putting results in copy of offsets and returning that
       // at the end of everything, we're just adding something to every double in the set by
@@ -164,7 +161,6 @@ apply_offsets(
       // don't need cell attributes, just location
       r[Location(static_cast<Idx>(y), static_cast<Idx>(x))].emplace_back(x, y);
     }
-    ++out;
   }
   return r;
 }
