1cf938094e1d4e41ab23ffe6950fc31557f20920
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-06-18 06:33:17 -0400

    define superclass for Offset and InnerPos and specialize them with template for bounds

--- src/cpp/InnerPos.h
+++ src/cpp/InnerPos.h
@@ -7,48 +7,66 @@
 #include "Cell.h"
 namespace fs
 {
-/**
- * \brief Offset from a position
- */
-struct Offset
+template <int XMin, int XMax, int YMin, int YMax>
+class BoundedPoint
 {
+protected:
+  using class_type = BoundedPoint<XMin, XMax, YMin, YMax>;
+  static constexpr auto INVALID_X = XMin - 1;
+  static constexpr auto INVALID_Y = YMin - 1;
+
 public:
   /**
-   * \brief Collection of Offsets
+   * \brief X direction (column)
    */
-  using OffsetSet = vector<Offset>;
+  inline constexpr InnerSize x() const noexcept { return x_; }
   /**
-   * \brief Offset in the x direction (column)
+   * \brief Y direction (row)
    */
-  inline constexpr double x() const noexcept { return coords_[0]; }
-  /**
-   * \brief Offset in the y direction (row)
-   */
-  inline constexpr double y() const noexcept { return coords_[1]; }
-  constexpr Offset(const double x, const double y) noexcept : coords_()
+  inline constexpr InnerSize y() const noexcept { return y_; }
+  constexpr BoundedPoint(const InnerSize x, const InnerSize y) noexcept : x_(x), y_(y)
   {
-    coords_[0] = x;
-    coords_[1] = y;
+#ifdef DEBUG_GRIDS
+    logging::check_fatal(
+      (INVALID_Y != y) && (y < YMin || y >= YMax), "y %f is out of bounds (%d, %d)", y, YMin, YMax
+    );
+    logging::check_fatal(
+      (INVALID_X != x) && (x < XMin || x >= XMax), "x %f is out of bounds (%d, %d)", x, XMin, XMax
+    );
+    logging::check_equal(x_, x, "x_");
+    logging::check_equal(y_, y, "y_");
+#endif
   }
-  constexpr Offset() noexcept : Offset(-1, -1) { }
-  constexpr Offset(Offset&& rhs) noexcept = default;
-  constexpr Offset(const Offset& rhs) noexcept = default;
-  Offset& operator=(const Offset& rhs) noexcept = default;
-  Offset& operator=(Offset&& rhs) noexcept = default;
+  constexpr BoundedPoint() noexcept : x_(XMin - 1), y_(YMin - 1) { }
+  constexpr BoundedPoint(class_type&& rhs) noexcept : BoundedPoint(rhs.x(), rhs.y()) { }
+  constexpr BoundedPoint(const class_type& rhs) noexcept : BoundedPoint(rhs.x(), rhs.y()) { }
+  class_type& operator=(const class_type& rhs) noexcept
+  {
+    x_ = rhs.x();
+    y_ = rhs.y();
+    return *this;
+  }
+  class_type& operator=(class_type&& rhs) noexcept
+  {
+    x_ = rhs.x();
+    y_ = rhs.y();
+    return *this;
+  }
   /**
    * \brief Multiply by duration to get total offset over time
    * \param duration time to multiply by
    */
-  constexpr Offset after(const double duration) const noexcept
+  template <class T>
+  constexpr T after(const DurationSize duration) const noexcept
   {
-    return Offset(x() * duration, y() * duration);
+    return static_cast<T>(class_type(x() * duration, y() * duration));
   }
   /**
    * \brief Less than operator
-   * \param rhs Offset to compare to
+   * \param rhs BoundedPoint to compare to
    * \return Whether or not this is less than the other
    */
-  bool operator<(const Offset& rhs) const noexcept
+  bool operator<(const class_type& rhs) const noexcept
   {
     if (x() == rhs.x())
     {
@@ -63,30 +81,53 @@ public:
   }
   /**
    * \brief Equality operator
-   * \param rhs Offset to compare to
+   * \param rhs BoundedPoint to compare to
    * \return Whether or not this is equivalent to the other
    */
-  bool operator==(const Offset& rhs) const noexcept { return (x() == rhs.x()) && (y() == rhs.y()); }
+  bool operator==(const class_type& rhs) const noexcept
+  {
+    return (x() == rhs.x()) && (y() == rhs.y());
+  }
   /**
    * \brief Add offset to position and return result
    */
-  [[nodiscard]] constexpr Offset add(const Offset o) const noexcept
+  template <class T, class O>
+  [[nodiscard]] constexpr T add(const O& o) const noexcept
   {
-    return Offset(x() + o.x(), y() + o.y());
+    return static_cast<T>(class_type(x() + o.x(), y() + o.y()));
   }
 
 private:
-  // coordinates as an array so we can treat an array of these as an array of doubles
-  double coords_[2];
+  InnerSize x_;
+  InnerSize y_;
+};
+/**
+ * \brief Offset from a position
+ */
+class Offset : public BoundedPoint<-1, 1, -1, 1>
+{
+public:
+  /**
+   * \brief Collection of Offsets
+   */
+  using OffsetSet = vector<Offset>;
+  using BoundedPoint<-1, 1, -1, 1>::BoundedPoint;
 };
 using OffsetSet = Offset::OffsetSet;
 // define multiplication in other order since equivalent
-constexpr Offset after(const double duration, const Offset& o) { return o.after(duration); }
-static constexpr MathSize x(const auto& p) { return p.x(); }
-static constexpr MathSize y(const auto& p) { return p.y(); }
+template <class T>
+constexpr T after(const DurationSize duration, const T& o)
+{
+  return o.after(duration);
+}
 /**
  * \brief The position within a Cell that a spreading point has.
  */
-using InnerPos = fs::Offset;
+class InnerPos : public BoundedPoint<0, MAX_COLUMNS, 0, MAX_ROWS>
+{
+  using BoundedPoint<0, MAX_COLUMNS, 0, MAX_ROWS>::BoundedPoint;
+};
+static constexpr MathSize x(const auto& p) { return p.x(); }
+static constexpr MathSize y(const auto& p) { return p.y(); }
 }
 #endif
