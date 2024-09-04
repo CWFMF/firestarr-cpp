136933c78918ce2c093d8b5c69cb5454f825be72
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-09-04 15:10:50 -0400

    don't cache unique points since only logging needs them twice

--- src/cpp/CellPoints.h
+++ src/cpp/CellPoints.h
@@ -109,7 +109,7 @@ public:
   }
   CellPoints&
   merge(const CellPoints& rhs);
-  const set<XYPos>&
+  set<XYPos>
   unique() const noexcept;
   bool
   operator<(const CellPoints& rhs) const noexcept;
@@ -148,9 +148,6 @@ private:
 public:
   pair<array_dists, array_pts> pts_;
 private:
-  mutable set<XYPos> pts_unique_;
-  // FIX: no point in atomic if not parallel, but need mutex if it is
-  mutable bool pts_dirty_;
   // use Idx instead of Location so it can be negative (invalid)
   Idx cell_x_;
   Idx cell_y_;
