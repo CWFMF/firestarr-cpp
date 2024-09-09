b5f0aa092292ad61fc1fa4751c7e9ad1219644f7
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-09-09 06:36:33 -0400

    don't check if src & dst are same before calling relativeIndex()

--- src/cpp/CellPoints.cpp
+++ src/cpp/CellPoints.cpp
@@ -306,7 +306,8 @@ CellPointsMap::insert(
 {
   CellPoints& cell_pts = insert(x, y);
   const Location& dst = cell_pts.location();
-  if (src != dst)
+  // adds 0 if the same so try without checking
+  // if (src != dst)
   {
     // we inserted a pair of (src, dst), which means we've never
     // calculated the relativeIndex for this so add it to main map
