57fc9e87b753fd28f67452837c0ed692bb32dbdb
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-11-07 09:44:14 -0500

    CHANGED: BUGFIX: OLD_CHANGED: fix trying to prevent dividing by 0 causing extreme rates of spread in grass on slopes

--- src/cpp/FBP45.h
+++ src/cpp/FBP45.h
@@ -604,7 +604,9 @@ public:
     const noexcept override
   {
     const auto mu = baseMultiplier(spread.nd(), *spread.weather());
-    return this->limitIsf(min(0.001, mu), calculateRos(mu, isi) * spread.slopeFactor());
+    // prevent divide by 0
+    const auto mu_not_zero = max(0.001, mu);
+    return this->limitIsf(mu_not_zero, calculateRos(mu, isi) * spread.slopeFactor());
   }
   /**
    * \brief Calculate rate of spread (m/min)
