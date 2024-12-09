b38372d8b3e3b1bb1d3c426945709d94770e50d4
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-12-09 13:02:07 -0500

    output spread within cells in directions that matter

--- src/cpp/InnerPos.h
+++ src/cpp/InnerPos.h
@@ -84,7 +84,15 @@ class InnerPos : public BoundedPoint<InnerSize, 0, 1, 0, 1>
  */
 class XYPos : public BoundedPoint<XYSize, 0, MAX_COLUMNS, 0, MAX_ROWS>
 {
+public:
   using BoundedPoint<XYSize, 0, MAX_COLUMNS, 0, MAX_ROWS>::BoundedPoint;
+
+  constexpr Location
+  location() const
+  {
+    // HACK: Location is (row, column) and this is (x, y)
+    return {static_cast<Idx>(second), static_cast<Idx>(first)};
+  }
 };
 
 /**
@@ -92,7 +100,15 @@ class XYPos : public BoundedPoint<XYSize, 0, MAX_COLUMNS, 0, MAX_ROWS>
  */
 class CellPos : public BoundedPoint<Idx, 0, MAX_COLUMNS, 0, MAX_ROWS>
 {
+public:
   using BoundedPoint<Idx, 0, MAX_COLUMNS, 0, MAX_ROWS>::BoundedPoint;
+
+  constexpr Location
+  location() const
+  {
+    // HACK: Location is (row, column) and this is (x, y)
+    return {static_cast<Idx>(second), static_cast<Idx>(first)};
+  }
 };
 
 static constexpr MathSize
