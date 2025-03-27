7e005543aaf00fe88e54448bbf35aceb6a2b831d
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-03-27 12:49:44 -0400

    fix having --curing argument twice

--- src/cpp/Main.cpp
+++ src/cpp/Main.cpp
@@ -502,13 +502,6 @@ main(
         "--deterministic",
         "Run deterministically (100% chance of spread & survival)"
       );
-      register_setter<size_t>(
-        &Settings::setStaticCuring,
-        "--curing",
-        "Specify static grass curing",
-        false,
-        &parse_size_t
-      );
       register_setter<
         ThresholdSize>(&Settings::setConfidenceLevel, "--confidence", "Use specified confidence level", false, &parse_value<ThresholdSize>);
       register_setter<string>(perim, "--perim", "Start from perimeter", false, &parse_string);
