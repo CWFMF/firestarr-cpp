7460b93072d22ff6c68ad50b5dc5ee34494772b2
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-04-04 08:30:33 -0400

    fix formatting and update copyright notice

--- src/cpp/LogPoints.cpp
+++ src/cpp/LogPoints.cpp
@@ -1,3 +1,7 @@
+/* Copyright (c) His Majesty the King in Right of Canada as represented by the Minister of Natural Resources, 2024-2025. */
+
+/* SPDX-License-Identifier: AGPL-3.0-or-later */
+
 #include "LogPoints.h"
 
 namespace tbd::sim
