13fd727df922150419857b2fdb17523b85a65988
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-04-04 08:30:33 -0400

    fix formatting and update copyright notice

--- src/cpp/SpreadAlgorithm.h
+++ src/cpp/SpreadAlgorithm.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2020,  Queen's Printer for Ontario */
-/* Copyright (c) His Majesty the King in Right of Canada as represented by the Minister of Natural Resources, 2024. */
+/* Copyright (c) His Majesty the King in Right of Canada as represented by the Minister of Natural Resources, 2024-2025. */
 
 /* SPDX-License-Identifier: AGPL-3.0-or-later */
 
