b14da3a99cb55f1c2fce7444cba9fe3395572088
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-05-28 15:44:10 -0400

    add mk_verbose.sh and change mk_clean.sh to pass arguments properly to cmake

--- scripts/mk_clean.sh
+++ scripts/mk_clean.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # didn't figure out how to do this with cmake yet but this works for now
 DIR_BUILD=/appl/firestarr/build
-VARIANT="$*"
-if [ -z "${VARIANT}" ]; then
-  VARIANT=Release
+VARIANT="${1}"
+if ( [ -z "${VARIANT}" ] || ( [ "Release" != "${VARIANT}" ] && [ "Debug" != ${VARIANT} ] ) ); then
+  # assume that argument is an arg to pass to cmake
+  VARIANT="Release"
+  echo "${@}"
+  # don't shift because $1 wasn't the variant
+else
+  shift;
 fi
+echo "${@}"
 echo Set VARIANT=${VARIANT}
 rm -rf ${DIR_BUILD} \
-  && /usr/bin/cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=${VARIANT} -S/appl/firestarr -B${DIR_BUILD} -G "Unix Makefiles" \
+  && /usr/bin/cmake --no-warn-unused-cli "${@}" -DCMAKE_BUILD_TYPE:STRING=${VARIANT} -S/appl/firestarr -B${DIR_BUILD} -G "Unix Makefiles" \
   && /usr/bin/cmake --build ${DIR_BUILD} --config ${VARIANT} --target all -j 50 --
---
+++ scripts/mk_verbose.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scripts/mk_clean.sh ${1} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
