08eb2ec92cc91fc46e56f586494dc4a931c7b7bb
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-06-05 06:34:53 -0400

    allow more than one iteration to run at a time, even if things that arene't used get computed, instead of waiting for each iteration to finish before proceeding

--- src/cpp/Model.cpp
+++ src/cpp/Model.cpp
@@ -981,17 +981,18 @@ Model::runIterations(
     );
     Model::task_limiter.set_limit(MAX_THREADS);
   }
-  // const auto MAX_CONCURRENT = std::max<size_t>(MAX_THREADS, 1);
-  // const auto concurrent_iterations = std::max<size_t>(
-  //   MAX_CONCURRENT / all_iterations[0].getScenarios().size(),
-  //   1);
+  const auto MAX_CONCURRENT = std::max<size_t>(MAX_THREADS, 1);
+  const auto concurrent_iterations = std::max<size_t>(
+    MAX_CONCURRENT / all_iterations[0].getScenarios().size(),
+    1
+  );
   // HACK: just set max of 4 for now
   // constexpr auto MIN_ITERATIONS_BEFORE_CHECK = 4;
   // const auto concurrent_iterations = std::min(
   //   static_cast<size_t>(MIN_ITERATIONS_BEFORE_CHECK),
   //   MAX_THREADS);
   // no point in running multiple iterations if deterministic
-  const auto concurrent_iterations = 1;
+  // const auto concurrent_iterations = 1;
   // const auto concurrent_iterations = MAX_THREADS;
   for (size_t x = 1; x < concurrent_iterations; ++x)
   {
