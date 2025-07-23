d0a8f065ff254e1fdf067c5f349555ae5e5c85c3
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-07-23 11:45:01 -0400

    added "Test" as a variant for build script that uses Release but with ASAN/UBSAN

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,13 @@
 DIR_ROOT="$(realpath $(dirname $(realpath "$0"))/..)"
 DIR_BUILD="${DIR_ROOT}/build"
 VARIANT="$1"
+ASAN_ARGS=
+if ( [ "Debug" == "${VARIANT}" ] || [ "Test" == "${VARIANT}" ] ); then
+  ASAN_ARGS=-DUSE_ASAN:BOOL=1
+fi
+if [ "Test" == "${VARIANT}" ]; then
+  VARIANT=Release
+fi
 if ( [ -z "${VARIANT}" ] || ( [ "Release" != "${VARIANT}" ] && [ "Debug" != ${VARIANT} ] ) ); then
   # assume that argument is an arg to pass to cmake
   VARIANT=Release
@@ -18,6 +25,7 @@ if [ "--clean-first" == "$1" ]; then
   shift;
 fi
 echo Set VARIANT=${VARIANT}
+echo Set ASAN_ARGS=${ASAN_ARGS}
 echo "${@}"
-/usr/bin/cmake --no-warn-unused-cli ${@} -DCMAKE_BUILD_TYPE:STRING=${VARIANT} -S${DIR_ROOT} -B${DIR_BUILD} -G "Unix Makefiles" \
+/usr/bin/cmake --no-warn-unused-cli ${ASAN_ARGS} ${@} -DCMAKE_BUILD_TYPE:STRING=${VARIANT} -S${DIR_ROOT} -B${DIR_BUILD} -G "Unix Makefiles" \
   && /usr/bin/cmake --build ${DIR_BUILD} --config ${VARIANT} --target all -j 50 ${CLEAN_FIRST} --
