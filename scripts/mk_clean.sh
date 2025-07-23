b6609034ca5a1fa791e222938d2355cd7dea8fe5
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-07-23 11:45:01 -0400

    added "Test" as a variant for build script that uses Release but with ASAN/UBSAN

--- scripts/mk_clean.sh
+++ scripts/mk_clean.sh
@@ -2,6 +2,13 @@
 # didn't figure out how to do this with cmake yet but this works for now
 DIR_BUILD=/appl/firestarr/build
 VARIANT="${1}"
+ASAN_ARGS=
+if ( [ "Debug" == "${VARIANT}" ] || [ "Test" == "${VARIANT}" ] ); then
+  ASAN_ARGS=-DASAN_ARGS:STRING="-fsanitize=address;-fsanitize=undefined"
+fi
+if [ "Test" == "${VARIANT}" ]; then
+  VARIANT="Release"
+fi
 if ( [ -z "${VARIANT}" ] || ( [ "Release" != "${VARIANT}" ] && [ "Debug" != ${VARIANT} ] ) ); then
   # assume that argument is an arg to pass to cmake
   VARIANT="Release"
@@ -12,6 +19,7 @@ else
 fi
 echo "${@}"
 echo Set VARIANT=${VARIANT}
+echo Set ASAN_ARGS=${ASAN_ARGS}
 rm -rf ${DIR_BUILD} \
-  && /usr/bin/cmake --no-warn-unused-cli "${@}" -DCMAKE_BUILD_TYPE:STRING=${VARIANT} -S/appl/firestarr -B${DIR_BUILD} -G "Unix Makefiles" \
+  && /usr/bin/cmake --no-warn-unused-cli ${ASAN_ARGS} "${@}" -DCMAKE_BUILD_TYPE:STRING=${VARIANT} -S/appl/firestarr -B${DIR_BUILD} -G "Unix Makefiles" \
   && /usr/bin/cmake --build ${DIR_BUILD} --config ${VARIANT} --target all -j 50 --
