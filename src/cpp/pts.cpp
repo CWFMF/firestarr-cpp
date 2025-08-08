651538c4804905ad1a0f1c877b27aa4037a47932
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-08-08 12:57:09 +0000

    only do lookup if required

--- src/cpp/pts.cpp
+++ src/cpp/pts.cpp
@@ -60,9 +60,9 @@ static constexpr InnerPos to_inner(const XYSize x, const XYSize y)
   const auto y0 = static_cast<DistanceSize>(modf(y, &integral));
   return {x0, y0};
 }
-CellPoints::CellPoints(const bool is_unburnable, const XYSize x, const XYSize y)
+CellPoints::CellPoints(const IntensityMap& intensity_map, const XYSize x, const XYSize y)
 {
-  if (!is_unburnable)
+  if (!intensity_map.cannotSpread(x, y))
   {
     points = make_unique<array_pts>();
     distances = make_unique<array_dists>();
@@ -116,14 +116,14 @@ set<XYPos> CellPoints::unique(
   return {it.begin(), it.end()};
 }
 void Points::insert(
-  const bool is_unburnable,
+  const IntensityMap& intensity_map,
   const XYSize x,
   const XYSize y)
 {
-  // HACK: try to insert nullptr and if that works modify
+  // No need to look up value in intensity_map if emplace doesn't happen
   auto p = map_.try_emplace(
     Location::hashXY(x, y),
-    is_unburnable,
+    intensity_map,
     x,
     y);
   p.first->second.insert(x, y);
