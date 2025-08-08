75bce3dab5a894d071823200bee80c718e6628b3
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-08-08 18:53:25 +0000

    fix problems with ownership

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -4,12 +4,14 @@ DIR_BUILD="${DIR_ROOT}/build"
 FILE_PERF=${DIR_PERF}/perf.data
 FREQ=100
 DIR_FG=/appl/FlameGraph
+sudo sysctl -w kernel.perf_event_paranoid=1
 [ ! -d "${DIR_PERF}" ] && mkdir -p "${DIR_PERF}"
 # don't make this an actual submodule because it's just this script that needs it
-[ ! -d "${DIR_FG}" ] && sudo git clone https://github.com/brendangregg/FlameGraph.git ${DIR_FG}
+[ ! -d "${DIR_FG}" ] && sudo apt install git && sudo git clone https://github.com/brendangregg/FlameGraph.git ${DIR_FG} && sudo chown -R ${USER}:${USER} ${DIR_FG}
 rm -rf ${DIR_BUILD}
-cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -S${DIR_ROOT} -B${DIR_BUILD} -G "Unix Makefiles"
-cmake --build ${DIR_BUILD} --config Debug --target all -j $(nproc) --
-time sudo perf record -o ${FILE_PERF} -F ${FREQ} -g --call-graph=dwarf -- $* > ${DIR_PERF}/run.log
-sudo chown ${USER}:${USER} ${FILE_PERF}
+cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -S${DIR_ROOT} -B${DIR_BUILD} -G "Unix Makefiles" > /dev/null 2>&1
+cmake --build ${DIR_BUILD} --config RelWithDebInfo --target all -j $(nproc) -- > /dev/null 2>&1
+cp ${DIR_BUILD}/tbd ${DIR_ROOT}/
+time perf record -o ${FILE_PERF} -F ${FREQ} -g --call-graph=dwarf -- $* > ${DIR_PERF}/run.log
+chown ${USER}:${USER} ${FILE_PERF}
 perf script -i ${FILE_PERF} | ${DIR_FG}/stackcollapse-perf.pl | ${DIR_FG}/flamegraph.pl > flame.html
