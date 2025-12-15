6ba8348134010481a07d690e70a5e0638e6cf853
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2025-12-15 19:17:13 +0000

    use logging for fwi test

--- src/cpp/fs/FwiReference.cpp
+++ src/cpp/fs/FwiReference.cpp
@@ -224,8 +224,7 @@ void FWIcalc(MathSize R, MathSize U, MathSize& fwi)
 int test_fwi_file(
   const char* file_in,
   const char* file_out,
-  const MathSize latitude = DEFAULT_LATITUDE,
-  const bool verbose = true
+  const MathSize latitude = DEFAULT_LATITUDE
 )
 {
   string line;
@@ -256,6 +255,7 @@ int test_fwi_file(
       return 1;
     }
   }
+  logging::debug("Testing FWI generated from %s for latitude %g", file_in, latitude);
   /* Main loop for calculating indices */
   while (getline(inputFile, line))
   {
@@ -290,10 +290,7 @@ int test_fwi_file(
     ffmc0_ = ffmc_;
     dmc0_ = dmc_;
     dc0_ = dc_;
-    if (verbose)
-    {
-      printf("%0.1f %0.1f %0.1f %0.1f %0.1f %0.1f\n", ffmc, dmc, dc, isi, bui, fwi);
-    }
+    logging::verbose("%0.1f %0.1f %0.1f %0.1f %0.1f %0.1f", ffmc, dmc, dc, isi, bui, fwi);
     if (nullptr != file_out)
     {
       outputFile << std::format(
@@ -312,7 +309,7 @@ int test_fwi_file(
 }
 int compare_files(const string file0, const string file1)
 {
-  printf("Comparing %s to %s\n", file0.c_str(), file1.c_str());
+  logging::info("Comparing %s to %s", file0.c_str(), file1.c_str());
   ifstream input0(file0.c_str());
   if (!input0.is_open())
   {
@@ -335,10 +332,9 @@ int compare_files(const string file0, const string file1)
     }
     if (auto cmp = line0.compare(line1); 0 != cmp)
     {
-      printf("\t%s\n!=\t%s\n", line0.c_str(), line1.c_str());
+      logging::error("\t%s\n!=\t%s", line0.c_str(), line1.c_str());
       return cmp;
     }
-    // printf("%s\n", line0.c_str());
   }
   if (getline(input1, line1))
   {
@@ -351,13 +347,13 @@ int test_fwi_files(const string file_expected, const string file_in, const strin
   // FIX: other tests use test/input but want to switch to test/data
   if (auto ret = test_fwi_file(file_in.c_str(), file_out.c_str()); ret != 0)
   {
-    printf("Generating %s from %s failed\n", file_out.c_str(), file_in.c_str());
+    logging::error("Generating %s from %s failed", file_out.c_str(), file_in.c_str());
     return ret;
   }
   if (auto cmp = compare_files(file_expected, file_out); 0 != cmp)
   {
-    printf(
-      "Comparison between files [%s, %s] gives %d\n", file_expected.c_str(), file_out.c_str(), cmp
+    logging::error(
+      "Comparison between files [%s, %s] gives %d", file_expected.c_str(), file_out.c_str(), cmp
     );
     return cmp;
   }
@@ -368,9 +364,15 @@ int test_fwi(const int argc, const char* const argv[])
   constexpr auto FILE_EXPECTED{"test/data/fwi/fwi_out.txt"};
   constexpr auto FILE_IN{"test/data/fwi/fwi_in.txt"};
   constexpr auto FILE_OUT{"test/output/fwi/fwi_out.txt"};
+  constexpr auto LATITUDE_MIN{-90.0};
+  constexpr auto LATITUDE_MAX{90.0};
+  constexpr auto STEP{0.001};
   // lets logging levels and help work
-  ArgumentParser parser{{.description = "Test FWI calculations"}, argc, argv};
+  ArgumentParser parser{
+    {.description = "Test FWI calculations"}, argc, argv, PositionalArgumentsRequired::NotRequired
+  };
   parser.parse_args();
+  logging::info("Testing FWI calculations");
   // no positional arguments
   parser.done_positional();
   make_directory_recursive("test/output/fwi");
@@ -378,25 +380,31 @@ int test_fwi(const int argc, const char* const argv[])
   {
     return ret;
   }
+  logging::info(
+    "Testing across latitude range [%+g, %+g] with step %g", LATITUDE_MIN, LATITUDE_MAX, STEP
+  );
   auto check_latitude = [](const MathSize latitude) {
-    printf("Comparing with latitude %f\n", latitude);
-    if (auto ret = test_fwi_file(FILE_IN, nullptr, latitude, false); ret != 0)
+    if (auto ret = test_fwi_file(FILE_IN, nullptr, latitude); ret != 0)
     {
-      printf("Generating from %s with latitude %f failed\n", FILE_IN, latitude);
+      logging::error("Generating from %s with latitude %g failed", FILE_IN, latitude);
       return ret;
     }
     return 0;
   };
-  constexpr auto RESOLUTION{0.001};
-  MathSize latitude = -90;
-  while (90 > latitude)
+  MathSize latitude = LATITUDE_MIN;
+  while (LATITUDE_MAX > latitude)
   {
     if (auto ret = check_latitude(latitude); 0 != ret)
     {
       return ret;
     }
-    latitude += RESOLUTION;
+    latitude += STEP;
   }
-  return check_latitude(90);
+  if (auto ret = check_latitude(LATITUDE_MAX); 0 != ret)
+  {
+    return ret;
+  }
+  logging::note("Testing FWI calculations succeeded");
+  return 0;
 }
 }
