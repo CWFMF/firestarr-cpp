c80f7e7b8ca9f8dce2205cc23de257d5ca3ccf5d
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2026-01-26 14:05:46 +0000

    move code into test_fwi.cpp

--- src/cpp/fs/TestFwi.h
+++
@@ -1,8 +0,0 @@
-// https://publications.gc.ca/collections/collection_2016/rncan-nrcan/Fo133-1-424-eng.pdf
-#ifndef FS_TEST_FWI_H
-#define FS_TEST_FWI_H
-namespace fs::testing
-{
-int test_fwi(const int argc, const char* const argv[]);
-}
-#endif
