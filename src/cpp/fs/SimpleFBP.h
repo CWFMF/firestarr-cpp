0f2b1196a767315656cebe14c017c2e6474ae3a7
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2026-02-12 18:52:23 +0000

    change SimpleFuelMixedWood to not be template

--- src/cpp/fs/SimpleFBP.h
+++ src/cpp/fs/SimpleFBP.h
@@ -729,7 +729,6 @@ public:
  * \tparam RosMultiplier Rate of spread multiplier * 10 [ST-X-3 eq 27/28, GLC-X-10 eq 29/30]
  * \tparam RatioMixed Percent conifer
  */
-template <int RosMultiplier, int RatioMixed>
 class SimpleFuelMixedWood : public SimpleFuelMixed
 {
 public:
@@ -744,7 +743,12 @@ public:
    * \param code Code to identify fuel with
    * \param name Name of the fuel
    */
-  constexpr SimpleFuelMixedWood(const FuelCodeSize& code, const char* name)
+  constexpr SimpleFuelMixedWood(
+    const FuelCodeSize& code,
+    const char* name,
+    const MathSize ros_multiplier,
+    const MathSize percent_mixed
+  )
     : SimpleFuelMixed(
         code,
         name,
@@ -753,8 +757,8 @@ public:
         282,
         150,
         50,
-        RosMultiplier,
-        RatioMixed,
+        ros_multiplier,
+        percent_mixed,
         108,
         25,
         50
@@ -1208,7 +1212,7 @@ public:
  * \tparam PercentConifer Percent conifer
  */
 template <int PercentConifer>
-class SimpleFuelM1 : public SimpleFuelMixedWood<10, PercentConifer>
+class SimpleFuelM1 : public SimpleFuelMixedWood
 {
 public:
   SimpleFuelM1() = delete;
@@ -1223,7 +1227,7 @@ public:
    * \param name Name of the fuel
    */
   constexpr SimpleFuelM1(const FuelCodeSize& code, const char* name)
-    : SimpleFuelMixedWood<10, PercentConifer>(code, name)
+    : SimpleFuelMixedWood(code, name, 10, PercentConifer)
   { }
 };
 /**
@@ -1231,7 +1235,7 @@ public:
  * \tparam PercentConifer Percent conifer
  */
 template <int PercentConifer>
-class SimpleFuelM2 : public SimpleFuelMixedWood<2, PercentConifer>
+class SimpleFuelM2 : public SimpleFuelMixedWood
 {
 public:
   SimpleFuelM2() = delete;
@@ -1246,7 +1250,7 @@ public:
    * \param name Name of the fuel
    */
   constexpr SimpleFuelM2(const FuelCodeSize& code, const char* name)
-    : SimpleFuelMixedWood<2, PercentConifer>(code, name)
+    : SimpleFuelMixedWood(code, name, 2, PercentConifer)
   { }
 };
 /**
