f1f2fc409da93020fc98a2cec4a7bbd7f15ae148
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2026-03-11 18:52:26 +0000

    fix build.sh not checking out proper vcpkg

--- scripts/build.sh
+++ scripts/build.sh
@@ -52,8 +52,11 @@ export PATH=$VCPKG_ROOT:$PATH
 export VCPKG_DISABLE_METRICS=1
 export VCPKG_INSTALLED_DIR=$VCPKG_ROOT/installed
 if [ ! -d "${VCPKG_ROOT}" ]; then
-  git clone https://github.com/microsoft/vcpkg.git
-  pushd vcpkg && ./bootstrap-vcpkg.sh -disableMetrics && popd
+  git clone -v https://github.com/microsoft/vcpkg.git \
+  && cd vcpkg \
+  && git checkout $(sed -n '/baseline/{s/.*\"\([^"]*\)",$/\1/g;p}' ../vcpkg-configuration.json) \
+  && cd .. \
+  && vcpkg/bootstrap-vcpkg.sh -disableMetrics
 fi
 # sudo apt-get install pkg-config
 set -x
