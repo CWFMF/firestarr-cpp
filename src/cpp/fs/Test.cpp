b5e3312a84721e262f69f4acbc38efedff250c3b
download.docker.com <jordan.evens@NRCan-RNCan.gc.ca>
2026-04-07 19:59:34 +0000

    fix warning: format not a string literal, argument types not checked

--- src/cpp/fs/Test.cpp
+++ src/cpp/fs/Test.cpp
@@ -78,70 +78,40 @@ public:
 };
 void showSpread(const SpreadInfo& spread, ptr<const FwiWeather> w, const FuelType* fuel)
 {
-  // column, (width, format)
-  // static const vector<const char* const> FMT_COLUMNS{
-  //   "PREC", "TEMP", "RH",  "WS",  "WD",  "FFMC", "DMC", "DC",  "ISI", "BUI", "FWI", "GS",  "SAZ",
-  //   "FUEL", "GC",   "L:B", "CBH", "CFB", "CFC",  "FD",  "HFI", "RAZ", "ROS", "SFC", "TFC",
-  // };
-  // static const vector<int> FMT_WIDTH{
-  //   5, 5, 3, 5, 3, 5, 5, 5, 5, 5, 5, 3, 3, 7, 3, 5, 4, 6, 6, 2, 6, 3, 6, 6, 6,
-  // };
-  static const vector<std::tuple<const char* const, int, const char* const>> FMT{
-    {"PREC", 5, "%*.2f"}, {"TEMP", 5, "%*.1f"}, {"RH", 3, "%*g"},    {"WS", 5, "%*.1f"},
-    {"WD", 3, "%*g"},     {"FFMC", 5, "%*.1f"}, {"DMC", 5, "%*.1f"}, {"DC", 5, "%*g"},
-    {"ISI", 5, "%*.1f"},  {"BUI", 5, "%*.1f"},  {"FWI", 5, "%*.1f"}, {"GS", 3, "%*d"},
-    {"SAZ", 3, "%*d"},    {"FUEL", 7, "%*s"},   {"GC", 3, "%*g"},    {"L:B", 5, "%*.2f"},
-    {"CBH", 4, "%*.1f"},  {"CFB", 6, "%*.3f"},  {"CFC", 6, "%*.3f"}, {"FD", 2, "%*c"},
-    {"HFI", 6, "%*ld"},   {"RAZ", 3, "%*d"},    {"ROS", 6, "%*.4g"}, {"SFC", 6, "%*.4g"},
-    {"TFC", 6, "%*.4g"},
+  // HACK: make two rows and then print so columns are aligned
+  std::stringstream ROW_HEADER{};
+  std::stringstream ROW_DATA{};
+  auto add_col = [&](const char* col, const string value) {
+    ROW_DATA << " " << value;
+    ROW_HEADER << std::format(" {:>{}s}", col, value.size());
   };
-  printf("Calculated spread is:\n");
-  // print header row
-  for (const auto& [col, width, fmt] : FMT)
-  {
-    // column width + 1 space
-    printf("%*s", width + 1, col);
-  }
-  printf("\n");
-  size_t cur_col{0};
-  auto print_col = [&](const char* col, auto value) {
-    const auto& [col_actual, width_fmt, fmt] = FMT.at(cur_col);
-    logging::check_fatal(
-      0 != strcmp(col, col_actual), "Expected column to be %s but got %s", col, col_actual
-    );
-    const auto width = width_fmt + 1;
-    printf(fmt, width, value);
-    ++cur_col;
-  };
-  // HACK: just do individual calls for now
-  // can we assign them to a lookup table if they're not all numbers?
-  print_col("PREC", w->prec.value);
-  print_col("TEMP", w->temperature.value);
-  print_col("RH", w->rh.value);
-  print_col("WS", w->wind.speed.value);
-  print_col("WD", w->wind.direction.value);
-  print_col("FFMC", w->ffmc.value);
-  print_col("DMC", w->dmc.value);
-  print_col("DC", w->dc.value);
-  print_col("ISI", w->isi.value);
-  print_col("BUI", w->bui.value);
-  print_col("FWI", w->fwi.value);
-  print_col("GS", spread.percentSlope());
-  print_col("SAZ", spread.slopeAzimuth());
+  add_col("PREC", std::format("{:5.2f}", w->prec.value));
+  add_col("TEMP", std::format("{:5.1f}", w->temperature.value));
+  add_col("RH", std::format("{:3g}", w->rh.value));
+  add_col("WS", std::format("{:5.1f}", w->wind.speed.value));
+  add_col("WD", std::format("{:3g}", w->wind.direction.value));
+  add_col("FFMC", std::format("{:5.1f}", w->ffmc.value));
+  add_col("DMC", std::format("{:5.1f}", w->dmc.value));
+  add_col("DC", std::format("{:5g}", w->dc.value));
+  add_col("ISI", std::format("{:5.1f}", w->isi.value));
+  add_col("BUI", std::format("{:5.1f}", w->bui.value));
+  add_col("FWI", std::format("{:5.1f}", w->fwi.value));
+  add_col("GS", std::format("{:3d}", spread.percentSlope()));
+  add_col("SAZ", std::format("{:3d}", spread.slopeAzimuth()));
   const auto simple_fuel = simplify_fuel_name(fuel->name());
-  print_col("FUEL", simple_fuel.c_str());
-  print_col("GC", fuel->grass_curing(spread.nd(), *w));
-  print_col("L:B", spread.lengthToBreadth());
-  print_col("CBH", fuel->cbh());
-  print_col("CFB", spread.crownFractionBurned());
-  print_col("CFC", spread.crownFuelConsumption());
-  print_col("FD", spread.fireDescription());
-  print_col("HFI", static_cast<size_t>(spread.maxIntensity()));
-  print_col("RAZ", spread.headDirection().asDegreesSize());
-  print_col("ROS", spread.headRos());
-  print_col("SFC", spread.surfaceFuelConsumption());
-  print_col("TFC", spread.totalFuelConsumption());
-  printf("\r\n");
+  add_col("FUEL", std::format("{:>7s}", simple_fuel));
+  add_col("GC", std::format("{:3.0g}", fuel->grass_curing(spread.nd(), *w)));
+  add_col("L:B", std::format("{:5.2f}", spread.lengthToBreadth()));
+  add_col("CBH", std::format("{:4.1f}", fuel->cbh()));
+  add_col("CFB", std::format("{:6.3f}", spread.crownFractionBurned()));
+  add_col("CFC", std::format("{:6.3f}", spread.crownFuelConsumption()));
+  add_col("FD", std::format("{:2c}", spread.fireDescription()));
+  add_col("HFI", std::format("{:6d}", static_cast<size_t>(spread.maxIntensity())));
+  add_col("RAZ", std::format("{:3d}", spread.headDirection().asDegreesSize()));
+  add_col("ROS", std::format("{:6.4g}", spread.headRos()));
+  add_col("SFC", std::format("{:6.4g}", spread.surfaceFuelConsumption()));
+  add_col("TFC", std::format("{:6.4g}", spread.totalFuelConsumption()));
+  printf("Calculated spread is:\n%s\n%s\n", ROW_HEADER.str().c_str(), ROW_DATA.str().c_str());
 }
 string generate_test_name(
   const auto& fuel,
@@ -260,13 +230,8 @@ string run_test_ignore_existing(
     output_directory, fuel_name, slope, aspect, num_hours, dc, dmc, ffmc, wind, final_sizes, true
   );
 }
-template <class V, class T = V>
-void show_options(
-  const char* name,
-  const vector<V>& values,
-  const char* fmt,
-  std::function<T(V&)> convert
-)
+template <class V>
+void show_options(const char* name, const vector<V>& values, std::function<string(V&)> convert)
 {
   printf("\t%ld %s: ", values.size(), name);
   // HACK: always print something before but avoid extra comma
@@ -275,8 +240,7 @@ void show_options(
   const char** p = &prefix_open;
   for (auto v : values)
   {
-    printf("%s", *p);
-    printf(fmt, convert(v));
+    printf("%s%s", *p, convert(v).c_str());
     p = &prefix_comma;
   }
   printf("]\n");
@@ -284,13 +248,11 @@ void show_options(
 template <class V>
 void show_options(const char* name, const vector<V>& values)
 {
-  return show_options<V, V>(name, values, "%d", [](V& value) { return value; });
+  return show_options<V>(name, values, [](V& value) { return std::format("{:d}", value); });
 };
 void show_options(const char* name, const vector<string>& values)
 {
-  return show_options<string, const char*>(name, values, "%s", [](string& value) {
-    return value.c_str();
-  });
+  return show_options<string>(name, values, [](string& value) { return value; });
 };
 int test(Settings& settings)
 {
