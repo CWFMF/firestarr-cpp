f8a5c5374046af47c6d8b71c6859a5acb830e961
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2026-04-09 15:05:09 +0000

    rename printf_centered

--- src/cpp/fs/debug_settings.cpp
+++ src/cpp/fs/debug_settings.cpp
@@ -16,41 +16,41 @@ void show_debug_settings()
   // FIX: duplicate code from settings file output
   std::string hr = std::format("{:#^{}}\n", "#", width);
   std::string side = std::format("{:#^{}}", "#", side_width);
-  auto printf_centered = [&](const std::string& text) {
+  auto print_centered = [&](const std::string& text) {
     std::cout << std::format("{}{: ^{}}{}\n", side, text, width - (2 * side_width), side);
   };
   std::cout << hr;
-  printf_centered("DEBUG OPTIONS");
+  print_centered("DEBUG OPTIONS");
   std::cout << hr;
 #ifndef NDEBUG
-  printf_centered("DEBUG");
+  print_centered("DEBUG");
 #endif
 #ifdef DEBUG_DIRECTIONS
-  printf_centered("DEBUG_DIRECTIONS");
+  print_centered("DEBUG_DIRECTIONS");
 #endif
 #ifdef DEBUG_FUEL_VARIABLE
-  printf_centered("DEBUG_FUEL_VARIABLE");
+  print_centered("DEBUG_FUEL_VARIABLE");
 #endif
 #ifdef DEBUG_FWI_WEATHER
-  printf_centered("DEBUG_FWI_WEATHER");
+  print_centered("DEBUG_FWI_WEATHER");
 #endif
 #ifdef DEBUG_GRIDS
-  printf_centered("DEBUG_GRIDS");
+  print_centered("DEBUG_GRIDS");
 #endif
 #ifdef DEBUG_POINTS
-  printf_centered("DEBUG_POINTS");
+  print_centered("DEBUG_POINTS");
 #endif
 #ifdef DEBUG_PROBABILITY
-  printf_centered("DEBUG_PROBABILITY");
+  print_centered("DEBUG_PROBABILITY");
 #endif
 #ifdef DEBUG_SIMULATION
-  printf_centered("DEBUG_SIMULATION");
+  print_centered("DEBUG_SIMULATION");
 #endif
 #ifdef DEBUG_STATISTICS
-  printf_centered("DEBUG_STATISTICS");
+  print_centered("DEBUG_STATISTICS");
 #endif
 #ifdef DEBUG_WEATHER
-  printf_centered("DEBUG_WEATHER");
+  print_centered("DEBUG_WEATHER");
 #endif
   std::cout << hr;
 #endif
