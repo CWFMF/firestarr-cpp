4fc6be46824e76aa2f019a81ed34dde5754d9f22
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2026-04-21 14:08:46 +0000

    reimplement trim_copy()

--- src/cpp/fs/Trim.h
+++ src/cpp/fs/Trim.h
@@ -2,41 +2,27 @@
 #ifndef FS_TRIM_H
 #define FS_TRIM_H
 #include "stdafx.h"
-// from https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
 namespace fs
 {
-/**
- * \brief Remove whitespace from left side of string
- * \param s string to trim
- */
-void trim_left(string* s);
-/**
- * \brief Remove whitespace from right side of string
- * \param s string to trim
- */
-void trim_right(string* s);
-/**
- * \brief Remove whitespace from both sides of string
- * \param s string to trim
- */
-void trim(string* s);
-/**
- * \brief Return new string with whitespace removed from left side
- * \param s string to trim
- * \return new string with whitespace removed from left side
- */
-[[nodiscard]] string trim_left_copy(string s);
-/**
- * \brief Return new string with whitespace removed from right side
- * \param s string to trim
- * \return new string with whitespace removed from right side
- */
-[[nodiscard]] string trim_right_copy(string s);
-/**
- * \brief Return new string with whitespace removed from both sides
- * \param s string to trim
- * \return new string with whitespace removed from both sides
- */
-[[nodiscard]] string trim_copy(string s);
+// remove leading and trailing spaces
+[[nodiscard]] inline string trim_copy(string s)
+{
+  int j = s.size() - 1;
+  while (j > 0 && ' ' == s.at(j))
+  {
+    --j;
+  }
+  if (0 >= j)
+  {
+    // string is empty or all whitespace
+    return "";
+  }
+  int i = 0;
+  while (i < j && ' ' == s.at(i))
+  {
+    ++i;
+  }
+  return s.substr(i, j - i + 1);
+}
 }
 #endif
