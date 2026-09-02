115289074429dafb323b5af8adeda55852fca679
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2026-09-02 19:32:42 +0000

    move to FuelTypeOld

--- src/cpp/fs/FireSpread.cpp
+++ src/cpp/fs/FireSpread.cpp
@@ -1,10 +1,10 @@
 /* SPDX-License-Identifier: AGPL-3.0-or-later */
 #include "FireSpread.h"
 #include "FuelOldLookup.h"
-#include "FuelType.h"
 #include "LookupTable.h"
 #include "Scenario.h"
 #include "Settings.h"
+#include "SimpleFuelType.h"
 #include "SpreadAlgorithm.h"
 #include "unstable.h"
 namespace fs
