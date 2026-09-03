3d9403f9506268fa643e98549ef344fa014f042e
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2026-09-03 12:51:05 +0000

    use FuelLookup.h

---
+++ src/cpp/fs/FuelLookup.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: AGPL-3.0-or-later */
+#ifndef FS_FUELLOOKUP_H
+#define FS_FUELLOOKUP_H
+// change imports in here instead of changing which files get imported to use FBP
+#include "FuelOldLookup.h"
+namespace fs::fuel
+{
+using LazyFuelLookup = LazyFuelOldLookup;
+}
+#endif
